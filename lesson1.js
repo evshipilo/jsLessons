64faa6ed3783776e7e945bc838f5bbcea16e2ce9
EVShipilo <evshipilo@gmail.com>
2020-01-23 14:32:59 +0300

    новый учебный проект

---
+++ lesson1.js
@@ -0,0 +1,14 @@
+"use strict";
+let name= prompt('ty hto?');
+let par=1;
+
+//if(age>=14 $$ age <=90)
+if (name===null) alert('otmena'); else {
+	if (name=='admin') {par=prompt('parol');
+	if (par===null) alert('otmena');else{
+		if(par=='boss') alert('welcome');
+		else alert('ne prav parol');
+	}
+					   }
+	else alert('govna kusok');
+}
