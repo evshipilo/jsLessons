4dd3d84258d79d7e6d85814f1a3df4f7862485a5
EVShipilo <evshipilo@gmail.com>
2020-01-23 14:49:16 +0300

    добавлен размер

--- lesson1.js
+++ lesson1.js
@@ -4,9 +4,9 @@ let par=1;
 
 //if(age>=14 $$ age <=90)
 if (name===null) alert('otmena'); else {
-	if (name=='admin') {par=prompt('parol');
+	if (name==='admin') {par=prompt('parol');
 	if (par===null) alert('otmena');else{
-		if(par=='boss') alert('welcome');
+		if(par==='boss') alert('welcome');
 		else alert('ne prav parol');
 	}
 					   }
