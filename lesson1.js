d1c3894f561b464e305db41013c62b9fa4d0943a
EVShipilo <evshipilo@gmail.com>
2020-01-24 11:19:40 +0300

    учебник java script задания параграфа 2.13

--- lesson1.js
+++ lesson1.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//2.12
+//2.12--------------------------
 /*
 
 let i=prompt("введите число больше 100");
@@ -9,6 +9,45 @@ while (i<100 && i){
 	i=prompt("введите число больше 100");
 }*/
 
-//prime numbers
+//prime numbers вывести простые числа от 1 до указанного числа---------------------
+/*let n = prompt("введите число");
+let string = "";
+gg: for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i - 1; j++) {
+        if (i % j == 0 && j != 1) continue gg;
+
+    }
+    string += i + ', ';
+
+}
+
+alert(string);*/
+
+//2.13----------------------
+
+/*
+let  b=prompt('введите браузер','');
+if (b=='edge') alert('ok'); else {
+    if (b=='opera'||b=='safari') alert('ok good'); else {
+        alert('we hope ok');
+    }
+}
+*/
+//-----------------------------------
+
+/*
+let number=+prompt('input number between 0 and 3');
+switch (number) {
+    case 0: alert('0'); break;
+    case 1: alert('1'); break;
+    case 2:
+    case 3: alert('2 or 3');
+
+}
+*/
+
+
+
+
 
 
